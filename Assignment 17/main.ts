f34f3cb97a7b1d5745565b0f8309587ce8d4974d
M Abdul Rehman <codinghamari@gmail.com>
2024-05-17 09:42:48 +0500

    Assignment 17 completed successfully

---
+++ Assignment 17/main.ts
@@ -0,0 +1,24 @@
+//Question 17: 
+// Shrinking Guest List: Unfortunately, your new table won’t arrive in time, and you can only invite two guests.
+
+//lets Code😉!
+
+
+let invitedGuests : string[] = ["Jamaal","Kamaal","Sawaal","Zawaal","Malaal","Jalaal","Halaal"];
+
+invitedGuests.pop();
+invitedGuests.shift();
+invitedGuests.splice(1,3)
+
+invitedGuests.forEach(invitedGuests => {
+    console.log(invitedGuests)
+});
+
+
+
+
+
+
+
+
+
