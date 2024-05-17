9744e2f276cd199f139ca8c33c784430e46f3ba9
M Abdul Rehman <codinghamari@gmail.com>
2024-05-17 10:49:33 +0500

    Assignment 18 completed

---
+++ Assignment 18/main.ts
@@ -0,0 +1,18 @@
+//Question 18: 
+//Seeing the World: Think of at least five places you’d like to visit.
+
+let visitingPlaces : string[] = ["Dubai","Singapure","Paris","London","Istanbul"];
+
+console.log("original list: ",visitingPlaces);
+
+visitingPlaces.sort();
+console.log("Ordered List: ",visitingPlaces);
+
+visitingPlaces.reverse();
+console.log("Unordered List: ",visitingPlaces);
+
+//console.log([...visitingPlaces].sort())
+
+
+
+
