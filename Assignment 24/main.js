2c429ec59d04d905d23dc8d83ccb2b1dd9c7d63c
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 11:22:23 +0500

    Assignment 24 Completed

---
+++ Assignment 24/main.js
@@ -0,0 +1,12 @@
+//Question 24:
+//More Conditional Tests: Expand your conditional tests to cover a wider range of comparisons.
+//Lets Code😉❗
+var compare1 = console.log("Mani" <= "Manni");
+var compare2 = console.log(true != true);
+var compare3 = console.log(20 >= 10);
+var compare4 = console.log("Abbas" >= "Abdul");
+console.log(compare1 == compare4);
+console.log(true && false);
+console.log(compare2 != compare2);
+console.log(compare3 >= compare1);
+
---
+++ Assignment 24/main.ts
@@ -0,0 +1,17 @@
+//Question 24:
+//More Conditional Tests: Expand your conditional tests to cover a wider range of comparisons.
+
+//Lets Code😉❗
+
+
+let compare1 : void = console.log ("Mani" <= "Manni"  );
+let compare2 : void = console.log (true != true);
+let compare3 : void = console.log (20>=10);
+let compare4 : void = console.log("Abbas" >= "Abdul");
+
+console.log (compare1 == compare4);
+console.log (true &&  false);
+console.log (compare2 != compare2);
+console.log (compare3 >= compare1);
+
+
