5bced0154134f1dcc0ae6e71096ecc963291c436
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 20:29:26 +0500

    Assignment 31 completed

---
+++ Assignment 31/main.ts
@@ -0,0 +1,10 @@
+//Question 31: 
+//No Users: Ensure your user list isn’t empty.
+
+// Lets Code😉❗
+
+let users : string[] = []
+if (users.length === 0 )
+    {console.log("Find users")}
+else
+    {console.log("Something went wrong")}
