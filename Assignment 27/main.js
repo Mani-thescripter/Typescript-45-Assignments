8e36d73324dcf63f4e0327130bc69dea47347b09
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 12:38:46 +0500

    assignment 27 completed

---
+++ Assignment 27/main.js
@@ -0,0 +1,36 @@
+//Question 27: 
+//Alien Colors #3: Convert your if-else chain to handle three colors: green, yellow, red.
+//Lets Code😉❗
+//Yellow alien version:
+var alienColors = "Yellow";
+if (alienColors === "Yellow") {
+    console.log("Congrats! you won");
+}
+else if (alienColors === "Green") {
+    console.log("Sorry! You lost");
+}
+else if (alienColors === "Red") {
+    console.log("Sorry! You lost");
+}
+//Green alien version:
+var alienColor = "Green";
+if (alienColor === "Yellow") {
+    console.log("Sorry! You lost");
+}
+else if (alienColor === "Green") {
+    console.log("Congrats! you won");
+}
+else if (alienColor === "red") {
+    console.log("Sorry! You lost");
+}
+//Red alien version:
+var aliensColor = "Red";
+if (aliensColor === "Yellow") {
+    console.log("Sorry! You lost");
+}
+else if (aliensColor === "Green") {
+    console.log("Sorry! You lost");
+}
+else if (aliensColor === "Red") {
+    console.log("Congrats! you won");
+}
