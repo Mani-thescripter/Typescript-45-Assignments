198ba81c938c4126fd3893ae657a7b8dd0056b3d
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 09:35:37 +0500

    Assignment 23 completed

---
+++ Assignment 23/main.js
@@ -0,0 +1,13 @@
+//Question 23: 
+//Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+//Lets Code😉❗
+var Pakistan = "8 words";
+var capOfPak = "Islamabad";
+console.log("how amny letters are in word Pakistan?");
+console.log(" I predict 8 letters are in word Pakistan", Pakistan === "8 words");
+console.log("how amny letters are in word Pakistan?");
+console.log("I predict 10 letters are in word Pakistan", Pakistan === "10 words");
+console.log("which city is the capital of Pakistan?");
+console.log("I predict Islamabad is the capital of Pakistan.", capOfPak === "Islamabad");
+console.log("which city is the capital of Pakistan?");
+console.log("I predict Karachi is the capital of Pakistan.", capOfPak === "Karachi");
---
+++ Assignment 23/main.ts
@@ -0,0 +1,25 @@
+//Question 23: 
+//Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+
+//Lets Code😉❗
+
+let  Pakistan : string = "8 words";
+let capOfPak : string  = "Islamabad";
+ 
+console.log("how amny letters are in word Pakistan?");
+console.log(" I predict 8 letters are in word Pakistan", Pakistan=== "8 words");
+
+
+console.log("how amny letters are in word Pakistan?");
+console.log("I predict 10 letters are in word Pakistan", Pakistan === "10 words");
+
+
+console.log("which city is the capital of Pakistan?");
+console.log("I predict Islamabad is the capital of Pakistan.", capOfPak === "Islamabad");
+
+console.log("which city is the capital of Pakistan?");
+console.log("I predict Karachi is the capital of Pakistan.", capOfPak === "Karachi");
+
+
+
+
