5cf49c8d23fefa9c42ae5ec3a877a19e8063cc73
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 14:15:26 +0500

    Assignment 28 completed (if-else)

---
+++ Assignment 28/main.js
@@ -0,0 +1,25 @@
+//Question 28: 
+//Stages of Life: Determine a person’s life stage with an if-else chain.
+//Lets Code😉❗
+var age = 30;
+if (age == 1 && age <= 4) {
+    console.log("baby child");
+}
+else if (age > 4 && age <= 10) {
+    console.log("child");
+}
+else if (age > 10 && age <= 20) {
+    console.log("Teenager");
+}
+else if (age > 20 && age < 30) {
+    console.log("Young");
+}
+else if (age > 29 && age <= 50) {
+    console.log("Adult");
+}
+else if (age > 50 && age <= 100) {
+    console.log(" old age ");
+}
+else {
+    console.log("Something went Wrong");
+}
