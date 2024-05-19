bbcde07a61bb27c3bc9cb358426f478f12a9971e
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 06:51:12 +0500

    Assignment 22 completed

---
+++ Assignment 22/main.js
@@ -0,0 +1,5 @@
+//Question 22: 
+//Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+var countries = ["Japan", "Canada", "New Zealand", "Iceland", "Switzerland"];
+console.log(countries[6]); //intentional error happened. cuz we print index number 6 which is not in an array
+console.log(countries[1] = "Canada");
---
+++ Assignment 22/main.ts
@@ -0,0 +1,8 @@
+//Question 22: 
+//Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+
+//Lets Code😉❗
+
+let countries: string[] = ["Japan", "Canada", "New Zealand", "Iceland", "Switzerland"]
+console.log (countries[6]);  //intentional error happened. cuz we print index number 6 which is not in an array
+console.log(countries[1] = "Canada")
