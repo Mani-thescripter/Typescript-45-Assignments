389de7f20f175e1ad063996d243fd808fba31b7e
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 15:41:12 +0500

    Assignment 29 completed

---
+++ Assignment 29/main.js
@@ -0,0 +1,7 @@
+//Question 29: 
+//Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+//Lets Code😉❗
+var myfavfruit = ["Apple", "Banana", "Mango", "Cherry", "Pineapple"];
+if (myfavfruit.push("Banana")) {
+    console.log("My Fav😋");
+}
---
+++ Assignment 29/main.ts
@@ -0,0 +1,9 @@
+//Question 29: 
+//Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+
+//Lets Code😉❗
+
+let myfavfruit : string[] = ["Apple","Banana","Mango","Cherry","Pineapple"];
+
+if (myfavfruit.push("Banana"))
+    {console.log("My Fav😋");}
