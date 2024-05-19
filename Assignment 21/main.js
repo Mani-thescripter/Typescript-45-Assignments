991c93ee43a7a7e0044e6f7edd0aaf2890c0ca93
M Abdul Rehman <codinghamari@gmail.com>
2024-05-19 06:37:02 +0500

    Assignment 21 comleted (Objects)

---
+++ Assignment 21/main.js
@@ -0,0 +1,9 @@
+//Question 21: 
+//Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+//Lets Code😉❗
+var book = {
+    name: "Baang-e-Daraa",
+    author: "Allama Muhammmad Iqbal",
+    subject: "Poetry"
+};
+console.log("Here is ".concat(book.name, ", which was wrote by ").concat(book.author, " and the format of this book is in ").concat(book.subject));
---
+++ Assignment 21/main.ts
@@ -0,0 +1,20 @@
+//Question 21: 
+//Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+//Lets Code😉❗
+
+let book : {
+    name : string,
+    author : string,
+    subject : string
+}= {
+    name : "Baang-e-Daraa",
+    author : "Allama Muhammmad Iqbal",
+    subject : "Poetry"
+}
+
+console.log(`Here is ${book.name}, which was wrote by ${book.author} and the format of this book is in ${book.subject}`)
+
+
+
